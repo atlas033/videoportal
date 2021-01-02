8aa99920b53e2269e4365fa59c1e5ca8da3765cd
atlas033 <philipp.ernstberger@hotmail.com>
2021-01-03 00:50:44 +0100

    #4

---
+++ test/readvideo.php
@@ -0,0 +1,30 @@
+<?php
+include("config.php");
+?>
+<!doctype html>
+<html>
+  <head>
+    <title>Upload and Store video to MySQL Database with PHP</title>
+  </head>
+  <body>
+    <div>
+ 
+     <?php
+     $fetchVideos = mysqli_query($con, "SELECT * FROM videos ORDER BY id DESC");
+     while($row = mysqli_fetch_assoc($fetchVideos)){
+       $location = $row['location'];
+       $name = $row['name'];
+       echo "<div style='float: left; margin-right: 5px;'>
+          <video src='".$location."' controls width='320px' height='320px' ></video>     
+          <br>
+          <span>".$name."</span>
+       </div>";
+     }
+     ?>
+ 
+    </div>
+    
+    <p><a href="index.php">Upload videos</a></p>
+
+  </body>
+</html>
