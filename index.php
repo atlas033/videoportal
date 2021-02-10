2a22e00edd3d460121a4bb2d8cafb9a498517cc9
Philipp Ernstberger <philipp.ernstberger@hotmail.com>
2021-02-10 20:44:08 +0100

    final commit

--- index.php
+++ index.php
@@ -1,6 +1,76 @@
 <?php
 include("config.php");
 
+if(isset($_POST['newVideo'])){
+  if(!isset($_FILES['video']['name']) && $_FILES['video']['name'] != ''){
+    $_SESSION['message'] = "Video fehlt.";
+  }  
+  else if (!isset($_POST['titel'])){
+    $_SESSION['message'] = "Titel fehlt.";
+  } 
+  else {
+
+    $extension = strtolower(pathinfo($_FILES['video']['name'],PATHINFO_EXTENSION));
+    
+    
+    $vid = file_get_contents($_FILES['video']['tmp_name']);
+    $vid = mysqli_real_escape_string($conn, $vid);
+    
+    $query = "INSERT INTO videos(titel, erstellungsdatum, regisseur, video, videoformat) VALUES('".$_POST['titel']."','".$_POST['erstellungsdatum']."','".$_POST['regisseur']."','".$vid."','".$extension."');";
+
+    mysqli_query($conn, $query);
+    $_SESSION['message'] = "Hochladen erfolgreich.". mysqli_error($conn);
+    
+    if (count($_POST) > 0) {
+      foreach ($_POST as $k=>$v) {
+          unset($_POST[$k]);
+      }
+    }
+    
+    header( "Location: {$_SERVER['REQUEST_URI']}", true, 303 );
+    exit();
+  }
+}
+
+
+if(isset($_POST['delete'])){
+  
+  mysqli_query($conn, "DELETE FROM videos WHERE id='". htmlspecialchars($_POST['id'])."'" );
+
+  $_SESSION['message']= "Eintrag gelöscht.";
+
+  unset($_POST['delete']);
+  unset($_POST['id']);
+
+  header( "Location: {$_SERVER['REQUEST_URI']}", true, 303 );
+  exit();
+}
+
+
+if(isset($_POST['editVideo'])){
+  if(isset($_POST['titel']) && $_POST['titel'] != ''){
+    $query="UPDATE videos SET titel='".$_POST['titel']."', erstellungsdatum='".$_POST['erstellungsdatum']."', regisseur='".$_POST['regisseur']."' WHERE ID=".$_POST['id'].";";
+  }else{
+    $query="UPDATE videos SET erstellungsdatum='".$_POST['erstellungsdatum']."', regisseur='".$_POST['regisseur']."' WHERE ID=".$_POST['id'].";";
+  }
+  mysqli_query($conn, $query);
+
+  $_SESSION['message'] = 'Eintrag geändert.';
+
+  if (count($_POST) > 0) {
+    foreach ($_POST as $k=>$v) {
+        unset($_POST[$k]);
+    }
+  }
+  
+  header( "Location: {$_SERVER['REQUEST_URI']}", true, 303 );
+  exit();
+}
+
+
+
+
+  
 ?>
 
 
@@ -18,15 +88,126 @@ include("config.php");
 <h1>NewTube - Ihr modernes Videoportal</h1>
 <p></p>
 
+<?php 
+  if(isset($_SESSION['message'])){
+       echo $_SESSION['message'];
+       unset($_SESSION['message']);
+    }
+?>
+<p></p>
+<br>
 
 <!-- neues video -->
 
-<form method="get" action="newvideo.php">
-    <button type="submit">neue Videos hochladen</button>
-</form>
+<button type="button" name="button" onclick="newVideo()">neues Video hochladen</button>
+
+<div id="newVideoDiv" style="display: none;">
+  <form class="" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" onsubmit="" enctype='multipart/form-data'>
+    <label>
+      <input type="text" name="titel" placeholder="Titel eingeben">
+    </label>
+    <label>
+      <input type="date" name="erstellungsdatum" value="">
+    </label>
+    <label>
+      <input type="text" name="regisseur" placeholder="Regisseur eingeben">
+    </label>
+    <label>
+      <input type="file" name="video" value="Upload">
+    </label>
+    <input type="submit" value="Video hochladen" name="newVideo"></input>
+  </form>
+</div>
 
 
-<hr>
+<!-- video bearbeiten -->
+
+<?php
+if(isset($_POST['edit'])){
+  $fetchVideos = mysqli_query($conn, "SELECT * FROM videos WHERE id='". htmlspecialchars($_POST['id'])."'");
+  $row = mysqli_fetch_assoc($fetchVideos);
+  
+  echo '<div id="editVideoDiv">
+  <hr>
+  <p>Bearbeiten der Videodetails</p>
+  <form class="" action="index.php" method="post">
+  <input type="hidden" name="id" value="'.htmlspecialchars($_POST['id']).'">
+    <label>
+      Titel:
+      <input type="text" value="'.$row["titel"].'" name="titel">
+    </label>
+    <label>
+      Erstellungsdatum:
+      <input type="date" value="'.$row["erstellungsdatum"].'" name="erstellungsdatum">
+    </label>
+    <label>
+      Regisseur:
+      <input type="text" value="'.$row["regisseur"].'" name="regisseur">
+    </label>
+    <input type="submit" value="bearbeiten" name="editVideo"></input>
+  </form>
+  </div>';
+
+  unset($_POST['edit']);
+  unset($_POST['id']);
+}
+?>
+
+
+<!-- video xml ansicht -->
+
+<?php
+  if(isset($_POST['xml'])){
+    $fetchVideos = mysqli_query($conn, "SELECT * FROM videos WHERE id=" .$_POST['id']);
+    $row = mysqli_fetch_assoc($fetchVideos);
+
+    $doc = new DOMDocument('1.0');
+
+    $doc->formatOutput = true;
+
+    $root = $doc->createElement('videoeintrag');
+    $root = $doc->appendChild($root);
+
+    $property = $doc->createElement('id');
+    $property = $root->appendChild($property);
+
+    $text = $doc->createTextNode($row['id']);
+    $text = $property->appendChild($text);
+
+    $property = $doc->createElement('titel');
+    $property = $root->appendChild($property);
+
+    $text = $doc->createTextNode($row['titel']);
+    $text = $property->appendChild($text);
+
+    $property = $doc->createElement('erstellungsdatum');
+    $property = $root->appendChild($property);
+
+    $text = $doc->createTextNode($row['erstellungsdatum']);
+    $text = $property->appendChild($text);
+
+    $property = $doc->createElement('regisseur');
+    $property = $root->appendChild($property);
+
+    $text = $doc->createTextNode($row['regisseur']);
+    $text = $property->appendChild($text);
+
+    $property = $doc->createElement('videoformat');
+    $property = $root->appendChild($property);
+
+    $text = $doc->createTextNode($row['videoformat']);
+    $text = $property->appendChild($text);
+
+
+    echo "<hr><p>XML-Ansicht des Eintrags</p><div> <pre> <code>";
+    echo htmlspecialchars($doc->saveXML());
+    echo "</code> </pre> </div>";
+
+    unset($_POST['xml']);
+    unset($_POST['id']);
+  }
+?>
+
 
 <!-- video ansicht -->
 
@@ -35,43 +216,40 @@ include("config.php");
   <?php
   $fetchVideos = mysqli_query($conn, "SELECT * FROM videos ORDER BY id DESC");
   while($row = mysqli_fetch_assoc($fetchVideos)){
+    $id = $row['id'];
     $video = $row['video'];
     $titel = $row['titel'];
+    $format = $row['videoformat'];
     echo "<div style='float: left; margin-right: 5px;'>
-      <video src='".$video."' controls width='320px' height='320px' ></video>     
-      <span>".$titel."</span>
-      <button type='button' name='button' onclick=''>Bearbeiten</button>
-      <button type='button' name='button' onclick=''>Löschen</button>
-      <button type='button' name='button' onclick=''>XML Ansicht</button>
+      <hr>
+      <video src='getVideo.php?id=".$id."' controls width='320px' height='320px' >
+      <p>Your browser cannot play the provided video file.</p>
+      </video>     
       <br>
+      <p>".$titel."</p>
+
+      <form action='index.php' method='post'>
+        <input type='hidden' name='id' value='".$id."'>
+        <input type='submit' value='Bearbeiten' name='edit'></input>
+      </form> 
+      
+      <form action='index.php' method='post'>
+        <input type='hidden' name='id' value='".$id."'>
+         <input type='submit' value='Löschen' name='delete'></input>
+      </form>
+
+      <form action='index.php' method='post'>
+        <input type='hidden' name='id' value='".$id."'>
+        <input type='submit' value='XML Ansicht' name='xml'></input>
+      </form>     
+    
       </div>";
   }
+
      
   ?>
  
 </div>
-
-<!-- // Ausgabe der Ergebnisse in HTML
-// echo "<table>";
-// while ($line = mysqli_fetch_array($result, MYSQL_ASSOC)) {
-//     echo "<tr>";
-//     foreach ($line as $col_value) {
-//         echo "<td>$col_value</td>";
-//     }
-//     echo "</tr>";
-//     echo "<hr>";
-// }
-// echo "</table>";
-
-// // Free resultset
-// mysql_free_result($result); -->
-
-
-
-
-<!-- video titel per php und datenbank anzeigen; dauer; abspielbutton;
-  neue zeile: xmlansicht button; edit button; loeschen button-->
-
 
 
 <!-- video player + darunter alle weiteren infos-->
@@ -93,7 +271,7 @@ function newVideo(){
 
 
 <?php
-  // $conn-> close();
+  $conn-> close();
 ?>
 </body>
 
